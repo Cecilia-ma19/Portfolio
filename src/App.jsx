e68368af7b5a7861467824c2336acc944de1ab99
Cecília Maria <cecilia.msaraujo9@gmail.com>
2025-03-26 22:50:00 -0300

    ajustes finais da página home, inserindo a imagem do avartar, e o texto principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
+import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
-import Profile from "./components/Profile/Profile";
-
+import Footer from "./components/Footer/Footer";
 function App() {
   return (
     <div>
-      <Profile />
       <Navbar />
+      <section className="container">
+        <div className="apresentacao">
+          <h1>
+            Olá! Eu sou a Cecília, <br />
+            <span>desenvolvedora Front-End.</span>
+          </h1>
+          <button className="btn">Saiba mais</button>
+        </div>
+        <figure>
+          <img className="img-home" src="/avatar.webp" alt="Imagem de Home" />
+        </figure>
+      </section>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Styles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={Styles.Footer}>
+      Feito por Cecília Maria 🤍 &copy; 2025
+    </footer>
+  );
+};
+
+export default Footer;
