e32199cff3ee090adb7c7ae5c0e3e6047b7d8034
Cecília Maria <cecilia.msaraujo9@gmail.com>
2025-04-11 15:43:54 -0300

    ajustando menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,25 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import Styles from "./Nav.module.css";
 import { Link } from "react-scroll";
+import { GiHamburgerMenu } from "react-icons/gi"; // ícone do hamburguer
 
 const Navbar = () => {
+  const [menuAberto, setMenuAberto] = useState(false);
+
   return (
     <header className={Styles.header}>
-      <span>Cecília Maria</span>
-      <nav className={Styles.nav}>
-        <Link to="home" smooth={true} duration={300}>
+      <span className={Styles.logo}>Cecília Maria</span>
+
+      <button
+        className={Styles.hamburguer}
+        onClick={() => setMenuAberto(!menuAberto)}
+      >
+        <GiHamburgerMenu />
+      </button>
+
+      <nav className={`${Styles.menu} ${menuAberto ? Styles.ativo : ""}`}>
+        <Link
+          to="home"
+          smooth={true}
+          duration={300}
+          onClick={() => setMenuAberto(false)}
+        >
           Home
         </Link>
-        <Link to="sobre" smooth={true} duration={300}>
+        <Link
+          to="sobre"
+          smooth={true}
+          duration={300}
+          onClick={() => setMenuAberto(false)}
+        >
           Sobre
         </Link>
-        <Link to="habilidades" smooth={true} duration={300}>
+        <Link
+          to="habilidades"
+          smooth={true}
+          duration={300}
+          onClick={() => setMenuAberto(false)}
+        >
           Habilidades
         </Link>
-        <Link to="projetos" smooth={true} duration={300}>
+        <Link
+          to="projetos"
+          smooth={true}
+          duration={300}
+          onClick={() => setMenuAberto(false)}
+        >
           Projetos
         </Link>
-        <Link to="contatos" smooth={true} duration={300}>
+        <Link
+          to="contatos"
+          smooth={true}
+          duration={300}
+          onClick={() => setMenuAberto(false)}
+        >
           Contatos
         </Link>
       </nav>
