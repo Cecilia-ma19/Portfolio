dfbb09e9225676dfd11801ac457af3728d7c6ece
Cecília Maria <cecilia.msaraujo9@gmail.com>
2025-04-16 14:46:38 -0300

    adicionando arrays para cards de projetos

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,24 +2,54 @@ import React from "react";
 import Style from "./Card.module.css";
 import { FaHtml5, FaCss3, FaReact, FaJs } from "react-icons/fa";
 
-const Card = ({ titulo, descricao, imagem }) => {
+const Card = ({
+  titulo,
+  descricao,
+  imagem,
+  linkProjeto,
+  linkRepo,
+  tecnologias,
+  video,
+}) => {
   return (
     <section className={Style.card}>
-      <img src={imagem} alt={titulo} />
+      {video ? (
+        <video
+          className={Style.cardVideo}
+          src={video}
+          autoPlay
+          loop
+          muted
+          playsInline
+        />
+      ) : (
+        <img src={imagem} alt={titulo} />
+      )}
+
       <h3>{titulo}</h3>
       <p>{descricao}</p>
       <div className={Style.card_footer}>
         <div className={Style.card_icones}>
-          <FaHtml5 title="Html" />
-          <FaCss3 title="Css" />
-          <FaJs title="JavaScript" />
-          <FaReact title="React" />
+          {tecnologias.map((tec, index) => {
+            switch (tec.toLowerCase()) {
+              case "html":
+                return <FaHtml5 key={index} title="HTML" />;
+              case "css":
+                return <FaCss3 key={index} title="CSS" />;
+              case "javascript":
+                return <FaJs key={index} title="JavaScript" />;
+              case "react":
+                return <FaReact key={index} title="React" />;
+              default:
+                return <span key={index}>{tec}</span>;
+            }
+          })}
         </div>
 
         <div className={Style.card_botoes}>
           <a
             className={Style.card_botao}
-            href="https://pagina-login-react-git-main-cecilia-ma19s-projects.vercel.app"
+            href={linkProjeto}
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -27,11 +57,11 @@ const Card = ({ titulo, descricao, imagem }) => {
           </a>
           <a
             className={Style.card_botao}
-            href="https://github.com/Cecilia-ma19/pagina-login-react.git"
+            href={linkRepo}
             target="_blank"
             rel="noopener noreferrer"
           >
-            Repositorio
+            Repositório
           </a>
         </div>
       </div>
--- src/pages/Projetos/Projetos.jsx
+++ src/pages/Projetos/Projetos.jsx
@@ -3,28 +3,41 @@ import Card from "../../components/Card/Card";
 import Style from "./Projetos.module.css";
 const projetos = [
   {
-    titulo: "Sistema de Login",
-    descricao: "Projeto com tela de login.",
+    titulo: "Login Page",
+    descricao: "Projeto com tela de login simples e responsiva.",
     imagem: "/pag-login.jpg",
-    linkProjeto: "https://meuprojeto1.com",
-    linkRepo: "https://github.com/seurepo1",
+    linkProjeto: "https://pagina-login-react.vercel.app",
+    linkRepo: "https://github.com/Cecilia-ma19/pagina-login-react",
+    tecnologias: ["HTML", "CSS", "JavaScript", "React"],
+  },
+  {
+    titulo: "Clone ChatGPT",
+    descricao: "Tela de login inspirada no layout do ChatGPT.",
+    video: "/clone-chat.mp4",
+    linkProjeto: "https://cecilia-ma19.github.io/ChatGPT-Clone-UI/",
+    linkRepo: "https://github.com/Cecilia-ma19/ChatGPT-Clone-UI",
+    tecnologias: ["HTML", "CSS"],
   },
 ];
-
 const Projetos = () => {
   return (
-    <section id="projetos" className={Style.cardsContainer}>
-      {projetos.map((item, index) => (
-        <Card
-          key={index}
-          titulo={item.titulo}
-          descricao={item.descricao}
-          imagem={item.imagem}
-          linkProjeto={item.linkProjeto}
-          linkRepo={item.linkRepo}
-        />
-      ))}
-    </section>
+    console.log(projetos),
+    (
+      <section id="projetos" className={Style.cardsContainer}>
+        {projetos.map((item, index) => (
+          <Card
+            key={index}
+            titulo={item.titulo}
+            descricao={item.descricao}
+            imagem={item.imagem}
+            video={item.video}
+            linkProjeto={item.linkProjeto}
+            linkRepo={item.linkRepo}
+            tecnologias={item.tecnologias}
+          />
+        ))}
+      </section>
+    )
   );
 };
 export default Projetos;
